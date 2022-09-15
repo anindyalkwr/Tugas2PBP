bc6cb3163ced5d524b31306a24b82d5cca8988fb
Anindya Lokeswara <anindya.lokeswara@ui.ac.id>
2022-09-15 09:13:32 +0700

    tests.py rev

--- katalog/tests.py
+++ katalog/tests.py
@@ -3,7 +3,7 @@ from katalog.models import CatalogItem
 
 # reference: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
 
-class YourTestClass(TestCase):
+class MyTestClass(TestCase):
     def setUp(self):
         self.firstItem = CatalogItem.objects.create(
             item_name = "Astrox 22 RX",
@@ -14,10 +14,10 @@ class YourTestClass(TestCase):
             item_url = "https://www.yonex.com/badminton/astrox-22-rx")
 
     def test_something_that_will_pass(self):
-        self.assertTrue((self.firstItem.item_name == 'Astrox 22 RX'), 'item_name is wrong')
+        self.assertTrue((self.firstItem.item_name == 'Astrox 22 RX'), 'Item name is wrong')
 
     def test_something_that_will_fail(self):
-        self.assertFalse((self.firstItem.item_price == 2500000), 'item price is correct')
+        self.assertFalse((self.firstItem.item_price == 2500000), 'Item price is correct')
 
-    def test_something(self):
+    def test_something_equal(self):
         self.assertEqual(self.firstItem.item_url, "https://www.yonex.com/badminton/astrox-22-rx")
