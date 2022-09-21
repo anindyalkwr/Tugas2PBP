e874e18c768c7fd0a26bc2983c3ad207c94df056
Anindya Lokeswara <anindya.lokeswara@ui.ac.id>
2022-09-22 00:47:48 +0700

    Implementes URLs unit testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client, override_settings
+from django.urls import resolve, reverse
+from whitenoise.storage import CompressedManifestStaticFilesStorage
 
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
 # Create your tests here.
+class TestURLs(TestCase):
+
+    def test_html_exists(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_xml_exist(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_json_exist(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template(self):
+        response = Client().get('/mywatchlist/')
+        self.assertTemplateUsed(response, 'mywatchlist.html')
+        self.assertTemplateUsed(response, 'base.html')
